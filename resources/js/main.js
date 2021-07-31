3e42f5af5777b23c0537025688ad387c0a90e8b9
camilapaleno <88165955+camilapaleno@users.noreply.github.com>
2021-07-30 21:21:10 -0700

    version 3

---
+++ resources/js/main.js
@@ -0,0 +1,32 @@
+const loader = document.querySelector('.loader');
+const main = document.querySelector('.main');
+
+function init() {
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        main.style.display = 'block';
+        setTimeout(() => (main.style.opacity = 1), 50);
+    }, 3000);
+}
+
+init();
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
