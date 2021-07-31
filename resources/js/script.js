f4c54b2ae8d1723ce5fca7c487dc41c414ad1a44
camilapaleno <88165955+camilapaleno@users.noreply.github.com>
2021-07-30 21:19:13 -0700

    version 2

---
+++ resources/js/script.js
@@ -0,0 +1,26 @@
+
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('camila'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'camila.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('computer'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'webdesignanim.json'
+})
+
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('camilamobile'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'camilamobile.json'
+})
+
